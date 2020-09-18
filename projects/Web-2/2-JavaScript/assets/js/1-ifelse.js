6abc3b12bfb91dbb5f82146ace693af04e31c69c
Linus Jakobsson <robotlinus123@gmail.com>
2020-09-18 15:07:57 +0200

    -m "first lesson of JavaScript learning"

---
+++ projects/Web-2/2-JavaScript/assets/js/1-ifelse.js
@@ -0,0 +1,24 @@
+// Greater then
+var age = 17;
+if (age >= 18) {
+    console.log('Du får ta körkortet.')
+} else {
+    console.log('Du får inte ta körkortet.')
+}
+
+// If somthing is ewual too
+var lesson = 'Web development';
+if (lesson == 'History') {
+    console.log('We have: ' + lesson);
+} else if (lesson == 'Web development') {
+    console.log('We have: ' + lesson)
+} else {
+    console.log('Spare time')
+}
+
+var isSunny = false;
+if(isSunny == true) {
+    console.log('Put on some shorts today.')
+} else {
+    console.log('Put on some warm clothes today.');
+}
