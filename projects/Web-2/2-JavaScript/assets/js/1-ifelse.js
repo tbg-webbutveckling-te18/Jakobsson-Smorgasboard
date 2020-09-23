4d5f80482820bb80a3d232069432560392c1754b
Linus Jakobsson <robotlinus123@gmail.com>
2020-09-23 11:54:44 +0200

    put if-statments iside of other if-statments

--- projects/Web-2/2-JavaScript/assets/js/1-ifelse.js
+++ projects/Web-2/2-JavaScript/assets/js/1-ifelse.js
@@ -6,7 +6,7 @@ if (age >= 18) {
     console.log('Du får inte ta körkortet.')
 }
 
-// If somthing is ewual too
+// If somthing is equal too
 var lesson = 'Web development';
 if (lesson == 'History') {
     console.log('We have: ' + lesson);
@@ -21,4 +21,34 @@ if(isSunny == true) {
     console.log('Put on some shorts today.')
 } else {
     console.log('Put on some warm clothes today.');
+}
+
+//step 1
+var d = new Date();
+if(d.getHours() <= 18){
+    console.log('Good day');
+} else if(d.getHours() >= 18){
+    console.log('Good evening');
+}
+
+//step 2
+if(d.getHours() < 10) {
+    console.log('Good morning')
+} else if (d.getHours() < 18) {
+    console.log('Good day');
+} else {
+    console.log('Good evenong');
+}
+
+//step 3
+if(d.getHours() < 10) {
+    if(d.getMonth() > 9 || d.getMonth < 2) {
+        console.log('Go ahead and sleep like a bear');
+    } else {
+        console.log('Good morning');
+    }
+} else if (d.getHours() < 18) {
+    console.log('Good day');
+} else {
+    console.log('Good evenong');
 }
