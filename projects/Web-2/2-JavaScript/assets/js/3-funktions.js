9703304695bfc6d0afefaab6256c0070ebd6abc3
Linus Jakobsson <robotlinus123@gmail.com>
2020-10-16 15:05:40 +0200

    made and worked whit functions

--- projects/Web-2/2-JavaScript/assets/js/3-funktions.js
+++ projects/Web-2/2-JavaScript/assets/js/3-funktions.js
@@ -31,3 +31,41 @@ function quadrupleMe(x) {
 }
 
 var myNumber = quadrupleMe(9);
+
+// Select an element
+var btnNumber = document.getElementById('myNumber');
+
+// Manipulate element
+btnNumber.innerHTML = '<strong>ey´´</strong>' + myNumber;
+
+// Funktion för första bokstaven stor
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+var city = 'stockholm';
+var capital = capitalize(city);
+
+document.writeln('<br> Capital of Sweden if: ' + capital);
+
+document.writeln('<br>');
+
+function isEven(x) {
+    if(x % 2 == 0){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+var x = isEven(2);
+document.writeln(x);
+
+document.writeln('<br>');
+
+function kebabToSnake(myString) {
+    return myString.replaceAll('-', '_');
+}
+
+var myString = kebabToSnake('Hej-på-dig-mask');
+document.writeln(myString);
