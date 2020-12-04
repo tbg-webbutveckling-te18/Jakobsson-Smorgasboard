f161616cb3dc12fab594ced4ed696a6fc31282d7
Linus Jakobsson <robotlinus123@gmail.com>
2020-12-04 15:05:59 +0100

    desided how to do the examples and began with if´s

---
+++ projects/Web-2/3-JSForN00bs/assets/js/examples.js
@@ -0,0 +1,27 @@
+/* Selektioner */
+var number = 11;
+if(number <= 10) {
+    // Den kod som är här körs
+} else if(number == 10) {
+    /* Om det åvan inte stämmer men detta
+    gör så körs den här biten av kod */
+} else {
+    /* Om inget annat stämmer 
+    så körs den här koden */
+}
+
+
+var letter = 'B';
+switch(letter) {
+    case 'A':
+        /* Om det stämmer så körs denna kod */
+        break;
+    case 'B':
+        // Annars så körs det här
+        break;
+    default:
+        // Om inget stämmer så blir det den här
+}
+
+
+/* Loopar */
