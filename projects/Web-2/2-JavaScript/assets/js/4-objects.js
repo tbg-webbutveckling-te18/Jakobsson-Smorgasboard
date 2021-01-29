d4974557ca0d366e9857f607212f1f1797cc29a0
Linus Jakobsson <robotlinus123@gmail.com>
2021-01-29 15:06:28 +0100

    more JS

--- projects/Web-2/2-JavaScript/assets/js/4-objects.js
+++ projects/Web-2/2-JavaScript/assets/js/4-objects.js
@@ -1,7 +1,7 @@
 var person = ['Sid', 33, 'Male', 'EagleShieldBay'];
  
 person.shift();
-document.writeln(person[2])
+// document.writeln(person[2])
  
 var personFriends = ['Bob', 'Dylan', 'Aiizee', 'Deezee'];
  
@@ -14,10 +14,18 @@ var user = {
     isEvil: false,
     shoutout: function() {
         alert(`Shoutout to y'all!`);
-    } 
+    }, 
+    ageing: function() {
+        return this.age++;
+    }
 };
  
-document.writeln(`<br>My name is ${user.name} and I am ${user.age} and I live in ${user.city}`);
+var btnAger = document.querySelector(".ager");
+btnAger.addEventListener(`click`, function() {
+    firstOutput.innerHTML = `<br>My name is ${user.name} and I am ${user.ageing()} and I live in ${user.city}`;
+});
+
+var firstOutput = document.getElementById("userInfo");
 // user.shoutout();
  
 for(var i = 0; i < user.friends.length; i++) {
@@ -29,9 +37,9 @@ user.friends.forEach(function(friend) {
     console.log(friend)
 })
  
-console.log(user.age)
-user.age++;
-console.log(user.age)
+// console.log(user.age)
+// user.age++;
+// console.log(user.age)
 
 var posts = [
     {
@@ -55,3 +63,9 @@ posts.forEach(function(post) {
     postOutput.innerHTML += `Author: ${post.author} <br> Commented ${post.comment} <br><br><br>`;
 });
 
+var input = document.querySelector(".updateCity");
+input.addEventListener("keydown", function(e) {
+    if(e.key === 'enter') {
+        alert('HAHA');
+    }
+});
