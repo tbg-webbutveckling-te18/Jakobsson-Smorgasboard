15c023a66fd6c30ac4c8f367432183e7dd3163a0
Linus Jakobsson <robotlinus123@gmail.com>
2021-03-03 11:53:39 +0100

    made changes to the lamps.

--- projects/Web-2/4-ResponsiveLayout/assets/js/phone.js
+++ projects/Web-2/4-ResponsiveLayout/assets/js/phone.js
@@ -3,6 +3,8 @@ var hamburger = document.querySelector('.mobileIcon');
 var menuClose = document.querySelector('.mobileIconClose');
 var menu = document.querySelector('.mobileMenu');
 
+var lamp = document.querySelectorAll('.lamp');
+
 // manipulate
 hamburger.addEventListener('click', function() {
     menu.classList.toggle('mebileMenuToggle')
@@ -11,3 +13,9 @@ hamburger.addEventListener('click', function() {
 menuClose.addEventListener('click', function() {
     menu.classList.toggle('mebileMenuToggle')
 });
+
+for(i = 0; i < lamp.length; i++) {
+    lamp[i].addEventListener('click', function() {
+        this.classList.toggle('focusing');
+    });
+}
