35b922adbbf9e51fb2726bd0c4a3c2af88da9669
Linus Jakobsson <robotlinus123@gmail.com>
2021-03-05 15:05:54 +0100

    placed a new pair of old images.

--- projects/Web-2/4-ResponsiveLayout/assets/js/phone.js
+++ projects/Web-2/4-ResponsiveLayout/assets/js/phone.js
@@ -3,7 +3,9 @@ var hamburger = document.querySelector('.mobileIcon');
 var menuClose = document.querySelector('.mobileIconClose');
 var menu = document.querySelector('.mobileMenu');
 
+// img select
 var lamp = document.querySelectorAll('.lamp');
+var dice = document.querySelectorAll('.dice');
 
 // manipulate
 hamburger.addEventListener('click', function() {
@@ -14,8 +16,14 @@ menuClose.addEventListener('click', function() {
     menu.classList.toggle('mebileMenuToggle')
 });
 
+// img manipulate
 for(i = 0; i < lamp.length; i++) {
     lamp[i].addEventListener('click', function() {
+        this.classList.toggle('focusing');
+    });
+}
+for(i = 0; i < dice.length; i++) {
+    dice[i].addEventListener('click', function() {
         this.classList.toggle('focusing');
     });
 }
