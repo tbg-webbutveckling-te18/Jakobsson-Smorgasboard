b1f4db09a87d79ea2551a8148b9708d5e0cb6c82
Linus Jakobsson <robotlinus123@gmail.com>
2021-06-02 11:50:05 +0200

    fixed the scroll and making buttons for scrolling.

---
+++ projects/Web-2/5-Slutprojekt-VT21/assets/js/scroll.js
@@ -0,0 +1,11 @@
+// variabler
+var btnLeft = document.querySelector('.scrollLeft');
+var btnRight = document.querySelector('.scrollRight');
+
+btnLeft.addEventListener('click', function(){
+    alert('hacking goverment servers.');
+});
+
+btnRight.addEventListener('click', function(){
+    alert('getting that betyg in webbutväckling');
+});
