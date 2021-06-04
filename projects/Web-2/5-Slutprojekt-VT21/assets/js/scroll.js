294fcc80e4fa6b28af0461956afa1f0cad3a1380
Linus Jakobsson <robotlinus123@gmail.com>
2021-06-04 13:40:07 +0200

    This might be my last push.

--- projects/Web-2/5-Slutprojekt-VT21/assets/js/scroll.js
+++ projects/Web-2/5-Slutprojekt-VT21/assets/js/scroll.js
@@ -2,10 +2,32 @@
 var btnLeft = document.querySelector('.scrollLeft');
 var btnRight = document.querySelector('.scrollRight');
 
+var scroller = document.querySelector('.cardParrent');
+
+var scrollCountInt = 0;
+var scrollCountStr = scrollCountInt.toString() + 'px';
+
+// events
 btnLeft.addEventListener('click', function(){
-    alert('hacking goverment servers.');
+    if (scrollCountInt <= -1200) {
+        scrollCountInt = 750;
+    }
+    
+    scrollCountInt -= 75;
+    scrollCountStr = scrollCountInt.toString() + 'px';
+
+    scroller.style.marginLeft = scrollCountStr;
+    console.log(scrollCountInt);
 });
 
 btnRight.addEventListener('click', function(){
-    alert('getting that betyg in webbutväckling');
+    if (scrollCountInt >= 750) {
+        scrollCountInt = -1200;
+    }
+
+    scrollCountInt += 75;
+    scrollCountStr = scrollCountInt.toString() + 'px';
+
+    scroller.style.marginLeft = scrollCountStr;
+    console.log(scrollCountInt);
 });
